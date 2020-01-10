ae1803f1cc3f583b5731b64cf3fce27d7ac718b2
Eduardo Cuducos <cuducos@users.noreply.github.com>
2020-01-09 21:51:10 -0500

    Adds type annotations

--- calculadora_do_cidadao.py
+++ calculadora_do_cidadao.py
@@ -1,8 +1,14 @@
 from datetime import date
 from re import sub
+from typing import Dict, Optional, Union
 
+from aiohttp import ClientSession
 from bs4 import BeautifulSoup
 from requests import post
+
+
+class ÍndiceInválido(Exception):
+    pass
 
 
 class BaseDaCalculadoraDoCidadão:
@@ -23,7 +29,16 @@ class BaseDaCalculadoraDoCidadão:
     }
     ÍNDICE_PADRÃO = "00189IGP-M"
 
-    def __init__(self, índice=None, verificar_ssl=True):
+    def __init__(
+        self, índice: Optional[str] = None, verificar_ssl: bool = True
+    ) -> None:
+        índice = índice or self.ÍNDICE_PADRÃO
+        if not índice in self.ÍNDICES:
+            válidos = ", ".join(self.ÍNDICES)
+            raise ÍndiceInválido(
+                f"{índice} não é um índice válido. Utilize um desses: {válidos}"
+            )
+
         self.verificar_ssl = verificar_ssl
         self.dados_do_formulário = {
             "aba": "1",
@@ -33,21 +48,21 @@ class BaseDaCalculadoraDoCidadão:
         }
 
     @staticmethod
-    def preparar_data(data=None):
+    def preparar_data(data: Optional[date] = None) -> str:
         data = data or date.today()
         return f"{data.month:0>2d}/{data.year}"
 
     @staticmethod
-    def limpar_chave(texto):
+    def limpar_chave(texto: str) -> str:
         texto = sub(r"\s+", " ", texto)
         return texto.strip()
 
     @staticmethod
-    def limpar_valor(texto):
+    def limpar_valor(texto: str) -> Union[str, date, float, None]:
         texto = sub(r"[^\d,/%]", "", texto)
 
         if "%" in texto:
-            return
+            return None
 
         if "," in texto:
             texto = texto.replace(",", ".")
@@ -59,8 +74,8 @@ class BaseDaCalculadoraDoCidadão:
 
         return texto
 
-    def parser(self, html):
-        html = BeautifulSoup(html, "html.parser")
+    def parser(self, content: str) -> Dict[str, Union[str, date, float]]:
+        html = BeautifulSoup(content, "html.parser")
         nós = html.find_all("td", class_="fundoPadraoAClaro3")
         textos = tuple(nó.text for nó in nós)
         dados = {
@@ -69,43 +84,43 @@ class BaseDaCalculadoraDoCidadão:
         }
         return {chave: valor for chave, valor in dados.items() if valor}
 
-    def dados_para_requisição(self, valor, data_original, data_final):
-        if not data_final or isinstance(data_final, date):
-            data_final = self.preparar_data(data_final)
-
-        if isinstance(data_original, date):
-            data_original = self.preparar_data(data_original)
-
-        valor = valor if isinstance(valor, float) else float(valor)
-        valor = f"{valor:.2f}".replace(".", ",")
-
+    def dados_para_requisição(
+        self, valor: float, data_original: date, data_final: date
+    ) -> Dict[str, str]:
         dados = self.dados_do_formulário.copy()
         dados.update(
             {
-                "dataInicial": data_original,
-                "dataFinal": data_final,
-                "valorCorrecao": valor,
+                "dataInicial": self.preparar_data(data_original),
+                "dataFinal": self.preparar_data(data_final),
+                "valorCorrecao": f"{valor:.2f}".replace(".", ","),
             }
         )
         return dados
 
 
 class CalculadoraDoCidadão(BaseDaCalculadoraDoCidadão):
-    def __call__(self, valor, data_original, data_final=None):
+    def __call__(
+        self, valor: float, data_original: date, data_final: Optional[date] = None
+    ) -> Dict[str, Union[str, float, date]]:
+        data_final_corrigida = data_final or date.today()
         resposta = post(
             self.URL_DO_FORMULÁRIO,
-            data=self.dados_para_requisição(valor, data_original, data_final),
+            data=self.dados_para_requisição(valor, data_original, data_final_corrigida),
             verify=self.verificar_ssl,
         )
         return self.parser(resposta.text)
 
 
 class CalculadoraDoCidadãoAsyncio(BaseDaCalculadoraDoCidadão):
-    async def __call__(self, sessão, valor, data_original, data_final=None):
-        kwargs = {
-            "data": self.dados_para_requisição(valor, data_original, data_final),
-            "ssl": self.verificar_ssl,
-        }
-
+    async def __call__(
+        self,
+        sessão: ClientSession,
+        valor: float,
+        data_original: date,
+        data_final: Optional[date] = None,
+    ) -> Dict[str, Union[str, float, date]]:
+        data_final_corrigida = data_final or date.today()
+        dados = self.dados_para_requisição(valor, data_original, data_final_corrigida)
+        kwargs = {"data": dados, "ssl": self.verificar_ssl}
         async with sessão.post(self.URL_DO_FORMULÁRIO, **kwargs) as resposta:
             return self.parser(await resposta.text())
